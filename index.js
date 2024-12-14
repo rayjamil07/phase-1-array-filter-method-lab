410013d16c381b916a916866ff2aa8385bf00e53
rayjamil <ryanwekesa019@gmail.com>
2024-12-14 09:26:24 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution here
+const drivers = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburg',
+    },
+    {
+        name: 'Sammy',
+        hometown: 'New York',
+    },
+    {
+        name: 'Sally',
+        hometown: 'Cleveland',
+    },
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles',
+    },
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    }
+];
+
+function findMatching(source, sought) {
+    return source.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(source, testString) {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+  }
+
+
+
